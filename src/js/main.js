0afc338f42f2232680a3d430461827b58102b52e
Anh Bui <buivananh2701@gmail.com>
2024-11-05 13:24:04 -0800

    Create Quiz result HTML +CSS

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const nextBtn = document.querySelectorAll('.next-btn');
     const previousBtn = document.querySelectorAll('.prev-btn');
     const steps = Array.from(document.querySelectorAll('.step'));
+    const submitBtn = document.querySelector('.submit-btn')
     const form = document.querySelector('form');
     const totalStep = 8;
 
@@ -24,11 +25,9 @@ document.addEventListener('DOMContentLoaded', () => {
             currentStep -= 1;
             console.log(`New step after clicked is: ${currentStep}`)
             moveStep();
-            progress( cell((currentStep+1)/totalStep * 100) );
-            console.log(`Progress: ${progress}%`)
+            progress( (currentStep+1)/totalStep * 100 );
         });
     });
-
 
     nextBtn.forEach(button => {
         button.addEventListener('click', () => {
@@ -36,16 +35,29 @@ document.addEventListener('DOMContentLoaded', () => {
             currentStep += 1;
             console.log(`New step after clicked is: ${currentStep}`)
             moveStep();
-            progress( cell((currentStep+1)/totalStep * 100) );
-            console.log(`Progress: ${progress}%`)
+            progress( (currentStep+1)/totalStep * 100 );
         });
     });
 
-    
     function moveStep() {
         steps.forEach(step => step.classList.remove('active'));
         steps[currentStep].classList.add('active');
     }
 
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const inputs = [];
+        form.querySelectorAll("input").forEach((input) => {
+            const {name,value} = input;
+            inputs.push({name,value});
+        });
+        console.log('Submission: ',inputs);
+        form.reset();
+    })
+    
+
+    submitBtn.addEventListener('click', () => {
+        window.location.assign('quiz-result.html');
+    })
 });
